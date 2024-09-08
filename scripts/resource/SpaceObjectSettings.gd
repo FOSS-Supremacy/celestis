eb76b077d1720e6fa533c333e5a1bcab9e274052
a6xdev <aa16.principal@gmail.com>
2024-09-08 10:03:27 -0300

    Added new functions to the resource

--- scripts/resource/SpaceObjectSettings.gd
+++ scripts/resource/SpaceObjectSettings.gd
@@ -1,4 +1,12 @@
 extends Resource
 class_name SpaceObjectResource
 
+@export_group("Object Settings")
 @export var object_mass:int = 10
+@export var gravity_strength: float = 1000.0
+
+@export_group("Orbit Settings")
+@export var orbiting: bool = false
+@export var orbit_radius: float = 20
+@export var orbit_speed: float = 50.0
+@export var orbit_delay:float = 1.0
