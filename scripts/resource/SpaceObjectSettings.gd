436954a5dc4dc7df71468b3fd77a62278268c5a5
a6xdev <aa16.principal@gmail.com>
2024-09-09 18:28:57 -0300

    new export var

--- scripts/resource/SpaceObjectSettings.gd
+++ scripts/resource/SpaceObjectSettings.gd
@@ -9,4 +9,4 @@ class_name SpaceObjectResource
 @export var orbiting: bool = false
 @export var orbit_radius: float = 20
 @export var orbit_speed: float = 50.0
-@export var orbit_delay:float = 1.0
+@export var pull_speed: float = 100.0  # Velocidade de puxar o objeto até o corpo central
