0b980e6a19dc67e93f4a5be8d823258c73691ce8
a6xdev <aa16.principal@gmail.com>
2024-09-14 08:13:23 -0300

    changes in code optimization

--- scripts/gameplay/systems/GravityController.gd
+++ scripts/gameplay/systems/GravityController.gd
@@ -3,7 +3,6 @@ class_name GravityController
 
 @export var gravity_strength: float = 1000.0
 var orbit_objects: Array = []
-
 var active_gravity: bool = false
 var object_center: Node2D
 
@@ -11,10 +10,11 @@ func _physics_process(delta: float) -> void:
 	if active_gravity and object_center:
 		for obj in orbit_objects:
 			gravity_controller(obj, delta)
-	else:
-		for obj in orbit_objects:
-			if obj:
-				obj.position += obj.velocity * delta
+
+func set_info(object_center: Node2D, pulled_objects: Array) -> void:
+	self.object_center = object_center
+	self.orbit_objects = pulled_objects
+	active_gravity = true
 
 func gravity_controller(pulled_object: CharacterBody2D, delta: float) -> void:
 	var distance = object_center.position.distance_to(pulled_object.position)
@@ -23,8 +23,6 @@ func gravity_controller(pulled_object: CharacterBody2D, delta: float) -> void:
 		var force = gravity_strength / distance
 		var acceleration = direction * force
 		pulled_object.apply_gravity(acceleration, delta)
-
-func set_info(object_center: Node2D, pulled_objects: Array) -> void:
-	self.object_center = object_center
-	self.orbit_objects = pulled_objects
-	active_gravity = true
+		
+		if pulled_object.has_method("active_orbit"):
+			pulled_object.active_orbit(object_center)
